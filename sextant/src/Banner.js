0ef3a97d34154defa5099a7d6d6e2a9c740649f7
Habou Zakaria <habouzakaria@gmail.com>
2023-10-02 17:55:50 +0100

    Stitch together a series of reusable React components to create a frontend for Sextant

---
+++ sextant/src/Banner.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Banner() {
+  return (
+    <div className="Banner">
+      <h1>Sextant</h1>
+    </div>
+  );
+}
+
+export default Banner;
---
+++ sextant/src/Exhibit.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Card from "./Card.js";
+
+export default function Exhibit () {
+    return (
+        <>
+            <h2>Exhibit</h2>
+            <Card />
+            <Card />
+        </>
+        
+    )
+}
